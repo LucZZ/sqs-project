3acb90ea186090d6bcb55c6c03b00370318148e6
studweislu8843 <lucas.weiss@stud.th-rosenheim.de>
2025-06-22 11:41:57 +0200

    switched to rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,22 @@
-# -- Project information -----------------------------------------------------
 project = 'sqs-project'
 copyright = '2025, Lucas Weiss'
 author = 'Lucas Weiss'
 
-# -- General configuration ---------------------------------------------------
-extensions = ['myst_parser']
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
